a4f939390136bd2a5088d56283f110bd96639595
Heitcleiff <89062666+Heitclieff@users.noreply.github.com>
2025-03-06 14:45:39 +0700

    Initial commit

---
+++ src/main/kotlin/dev/ku/ku_routing/KuRoutingApplication.kt
@@ -0,0 +1,11 @@
+package dev.ku.ku_routing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KuRoutingApplication
+
+fun main(args: Array<String>) {
+	runApplication<KuRoutingApplication>(*args)
+}
---
+++ src/test/kotlin/dev/ku/ku_routing/KuRoutingApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.ku.ku_routing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KuRoutingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
